be24d073e68f5164dc26916338fb862081304893
LamDorcas0320 <dorcaslam2003@yahoo.com.hk>
2026-06-10 22:59:42 +0800

    update router

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from "react";
-import BrowserRouter from "react-router-dom/BrowserRouter";
+import { HashRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
@@ -7,9 +7,9 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter base="/">
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
 );
 
 reportWebVitals();
