b179252174f6aecab185643d7d3fde100eb45249
LamDorcas0320 <dorcaslam2003@yahoo.com.hk>
2026-06-10 22:57:23 +0800

    main.jsx update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import BrowserRouter from "react-router-dom/BrowserRouter";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import reportWebVitals from "./reportWebVitals";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>,
+);
+
+reportWebVitals();
